0faa1b3eab77d3d1489595bc27ca1ef602f987c0
Rafael Rodriguez <>
2018-01-29 11:35:32 -0600

    adding uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
     entry: './scripts/main.js',
     output: {
         path: __dirname,
         filename: './scripts/main.min.js'
     },
-    watch: true
+    watch: true,
+    plugins: [
+        new UglifyJsPlugin({
+            test: /\.js($|\?)/i
+        })
+    ]
 }
